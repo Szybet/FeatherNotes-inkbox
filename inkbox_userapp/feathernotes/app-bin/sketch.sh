77a064379267493aef3ff758ce29a7c3afddd8de
Szybet <53944559+Szybet@users.noreply.github.com>
2023-09-09 13:31:32 +0200

    fix sketch if mupdf incompability

--- inkbox_userapp/feathernotes/app-bin/sketch.sh
+++ inkbox_userapp/feathernotes/app-bin/sketch.sh
@@ -1,3 +1,4 @@
 #!/system-bin/sh
 cd /app-bin
-env PRODUCT=luna PATH="/app-bin:/system-bin" LD_LIBRARY_PATH="/system-lib/lib:/system-lib/qt/lib:/app-lib" /system-lib/lib/ld-linux-armhf.so.3 ./sketch-bin /app-data
+# Remember app lib before the rest
+env PRODUCT=luna PATH="/app-bin:/system-bin" LD_LIBRARY_PATH="/app-lib:/system-lib/lib:/system-lib/qt/lib" /system-lib/lib/ld-linux-armhf.so.3 ./sketch-bin /app-data
